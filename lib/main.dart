3680af64f2384141e001b6fd0748eee8874607de
fanicahyo7 <fanidc7@gmail.com>
2020-12-09 16:35:15 +0700

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:submission_flutter_pemula_dicoding_bookstore/model/book.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MenuBuku(),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
 
-class MenuBuku extends StatelessWidget {
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    List<Book> books = masterBuku.sublist(0, 5);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          title: Center(
+            child: Image.network(
+              'https://2.bp.blogspot.com/-aMD84HEzaSo/XApL1x34yRI/AAAAAAAAQtM/-rJAAUExGlM3kYfFc8CJ0O6GTC0suYCWQCLcBGAs/s320/Togamas.png',
+              fit: BoxFit.cover,
+              height: 150,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(16, 16, 16, 10),
+            child: Text('Buku Populer'),
+          ),
+          Container(
+              height: 250,
+              width: 400,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: books
+                    .map((e) => Container(
+                        margin: EdgeInsets.only(left: 16),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius: 3,
+                                  color: Colors.black12,
+                                  blurRadius: 15)
+                            ]),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(e.picture),
+                                      fit: BoxFit.scaleDown)),
+                            ),
+                            Container(
+                                margin: EdgeInsets.fromLTRB(12, 12, 12, 6),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(e.judul),
+                                    Text(e.harga.toString())
+                                  ],
+                                )),
+                          ],
+                        )))
+                    .toList(),
+              )),
+          Container(
+            margin: EdgeInsets.fromLTRB(16, 16, 16, 10),
+            child: Text('Semua Buku'),
+          ),
+        ])));
   }
 }
---
+++ lib/model/book.dart
@@ -0,0 +1,91 @@
+class Book {
+  final String judul;
+  final String penyusun;
+  final String tahunterbit;
+  final String halaman;
+  final int harga;
+  final double rate;
+  final String picture;
+
+  Book(
+      {this.judul,
+      this.penyusun,
+      this.tahunterbit,
+      this.halaman,
+      this.harga,
+      this.rate,
+      this.picture});
+}
+
+List<Book> masterBuku = [
+  Book(
+      judul: 'PANDUAN PSIKOTES MUDAH & PRAKTIS',
+      penyusun: 'SUJONO SUMARJONO',
+      tahunterbit: '2008/12',
+      halaman: '162',
+      harga: 20000,
+      picture: 'assets/panduan-psikotes.jpg'),
+  Book(
+      judul: 'STOP MAKING MY HEART FLUTTER !',
+      penyusun: 'NANA SHIIBA',
+      tahunterbit: '2018/09',
+      halaman: '80',
+      harga: 25000,
+      picture: 'assets/stop-making-my-heart-flutter.jpg'),
+  Book(
+      judul: 'PEMROGRAMAN DATABASE VISUAL BASIC .NET',
+      penyusun: 'YUSWANTO,SUBARI,',
+      tahunterbit: '2006/08',
+      halaman: '428',
+      harga: 48000,
+      picture: 'assets/vb.net.jpg'),
+  Book(
+      judul: 'PEMROGRAMAN JAVA',
+      penyusun: 'RIJALUL FIKRI,IPAM FUADINA ADAM,IMAM PRAKOSO,',
+      tahunterbit: '2005/12',
+      halaman: '388',
+      harga: 55500,
+      picture: 'assets/pemrograman-java.jpg'),
+  Book(
+      judul: 'PEMROGRAMAN PHP 7 UNTUK PEMULA',
+      penyusun: 'JUBILEE ENTERPRISE',
+      tahunterbit: '2017/04',
+      halaman: '151',
+      harga: 36800,
+      picture: 'assets/pemrograman-php7.jpg'),
+  Book(
+      judul: 'PEMROGRAMAN ANDROID & DATABASE',
+      penyusun: 'ABDUL KADIR',
+      tahunterbit: '2018/05',
+      halaman: '500',
+      harga: 94800,
+      picture: 'assets/android-database.jpg'),
+  Book(
+      judul: 'DASAR PEMROGRAMAN C++',
+      penyusun: 'HANIF AL FATTA, S.Kom.',
+      tahunterbit: '2006/07',
+      halaman: '106',
+      harga: 21000,
+      picture: 'assets/c++.jpg'),
+  Book(
+      judul: 'DASAR PEMROGRAMAN DELPHI',
+      penyusun: 'ABDUL KADIR',
+      tahunterbit: '2006/01',
+      halaman: '579',
+      harga: 15000,
+      picture: 'assets/delphi.jpg'),
+  Book(
+      judul: '5 PEMROGRAMAN DESAIN WEBSITE',
+      penyusun: 'JUBILEE ENTERPRISE',
+      tahunterbit: '2019/11',
+      halaman: '320',
+      harga: 8000,
+      picture: 'assets/desain-website.jpg'),
+  Book(
+      judul: 'PEMROGRAMAN BAHASA ASSEMBLY',
+      penyusun: 'MAMAN ABDUROHMAN',
+      tahunterbit: '2010/12',
+      halaman: '260',
+      harga: 842500,
+      picture: 'assets/assembly.jpg')
+];
